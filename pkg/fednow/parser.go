5e8380f7296b6118141b8e27e792b76065ae8733
thanay-mbanq <thanay.murthy@mbanq.com>
2026-01-13 11:36:32 -0800

    Added camt.056 messsage functionality

--- pkg/fednow/parser.go
+++ pkg/fednow/parser.go
@@ -8,11 +8,13 @@ import (
 	"strings"
 
 	admi002 "github.com/mbanq/iso20022-go/ISO20022/admi_002_001_01"
+	camt056 "github.com/mbanq/iso20022-go/ISO20022/camt_056_001_08"
 	head "github.com/mbanq/iso20022-go/ISO20022/head_001_001_02"
 	pacs002 "github.com/mbanq/iso20022-go/ISO20022/pacs_002_001_10"
 	pacs008 "github.com/mbanq/iso20022-go/ISO20022/pacs_008_001_08"
 	pain013 "github.com/mbanq/iso20022-go/ISO20022/pain_013_001_07"
 	"github.com/mbanq/iso20022-go/pkg/fednow/admi"
+	"github.com/mbanq/iso20022-go/pkg/fednow/camt"
 	"github.com/mbanq/iso20022-go/pkg/fednow/pacs"
 	"github.com/mbanq/iso20022-go/pkg/fednow/pain"
 )
@@ -77,6 +79,12 @@ func Parse(xmlData []byte) (FedNowMessage, error) {
 			return nil, err
 		}
 		fednowMsg, err = pain.ParsePain013(appHdr, doc)
+	case strings.Contains(msgType, "camt.056.001.08"):
+		var doc camt056.Document
+		if err = decoder.Decode(&doc); err != nil {
+			return nil, err
+		}
+		fednowMsg, err = camt.ParseCamt056(appHdr, doc)
 	default:
 		return nil, errors.New("unsupported message type: " + msgType)
 	}
